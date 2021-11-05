7cc5aa07531e80bd6f673dc3723fb3af9dddd998
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2021-11-05 18:00:44 +0000

    Add favicon and final tidy up for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener("DOMContentLoaded",function() {
         })
     }
 
+    /*makes it so you can press enter for the answer*/
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded",function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value="";
+    document.getElementById("answer-box").focus();
 
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() *25) + 1;
